3f3c3a047636883d661d1079395c71f87436f024
单琢 <shanzhuo@vpgame.cn>
2023-09-26 22:14:34 +0800

    3

--- untar/untar.go
+++ untar/untar.go
@@ -2,64 +2,24 @@ package untar
 
 import (
 	"archive/tar"
+	"compress/bzip2"
 	"compress/gzip"
+	"errors"
 	"io"
-	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 )
 
-type UnTar struct {
-	Source string `json:"source"`
-	Target string `json:"target"`
+type TarBoll struct {
+	Source string
+	Target string
 }
 
-func (t *UnTar) saveFile(fw *os.File, tr *tar.Reader) error {
-	defer fw.Close()
-	_, err := io.Copy(fw, tr)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (t *UnTar) detFileType(f *os.File) string {
-	buff := make([]byte, 512)
-
-	_, err := f.Read(buff)
-	if err != nil {
-		panic(err)
-	}
-	filetype := http.DetectContentType(buff)
-
-	f.Seek(0, io.SeekStart)
-	return filetype
-}
-
-func (t *UnTar) UnTarGzip() error {
-	var tarReader *tar.Reader
-	fw, err := os.Open(t.Source)
-	if err != nil {
-		return err
-	}
-	defer fw.Close()
-
-	if fileType := t.detFileType(fw); fileType == "application/x-gzip" {
-		// read gzip
-		gr, err := gzip.NewReader(fw)
-		if err != nil {
-			return err
-		}
-		defer gr.Close()
-
-		tarReader = tar.NewReader(gr)
-	} else {
-		tarReader = tar.NewReader(fw)
-	}
+func (t *TarBoll) loopAndSave(tarReader *tar.Reader) error {
 
 	// create target dir
-
-	if _, err = os.Stat(t.Target); err != nil {
+	if _, err := os.Stat(t.Target); err != nil {
 		if err = os.MkdirAll(t.Target, 0755); err != nil {
 			return err
 		}
@@ -93,6 +53,78 @@ func (t *UnTar) UnTarGzip() error {
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func (t *TarBoll) UnTar() error {
+
+	switch s := path.Ext(t.Source); s {
+
+	case ".tar":
+		return t.tar()
+	case ".gz":
+		return t.tgz()
+	case ".tgz":
+		return t.tgz()
+	case ".bz2":
+		return t.bz2()
+	default:
+		err := errors.New("invalid file type")
+		return err
+	}
+}
+
+func (t *TarBoll) tar() error {
+	var tarReader *tar.Reader
+	fw, err := os.Open(t.Source)
+	if err != nil {
+		return err
+	}
+	defer fw.Close()
+	tarReader = tar.NewReader(fw)
+
+	return t.loopAndSave(tarReader)
+}
+
+func (t *TarBoll) bz2() error {
+
+	fw, err := os.Open(t.Source)
+	if err != nil {
+		return err
+	}
+	defer fw.Close()
+
+	br := bzip2.NewReader(fw)
+	tarReader := tar.NewReader(br)
+	return t.loopAndSave(tarReader)
+}
+
+func (t *TarBoll) tgz() error {
+	var tarReader *tar.Reader
+	fw, err := os.Open(t.Source)
+	if err != nil {
+		return err
+	}
+	defer fw.Close()
+
+	// read gzip
+	gr, err := gzip.NewReader(fw)
+	if err != nil {
+		return err
+	}
+	defer gr.Close()
+
+	tarReader = tar.NewReader(gr)
+
+	return t.loopAndSave(tarReader)
+}
+
+func (t *TarBoll) saveFile(fw *os.File, tr *tar.Reader) error {
+	defer fw.Close()
+	_, err := io.Copy(fw, tr)
+	if err != nil {
+		return err
 	}
 	return nil
 }
